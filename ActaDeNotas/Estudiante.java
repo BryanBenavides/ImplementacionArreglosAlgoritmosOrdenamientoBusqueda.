db744a4da002fe0f7010c39bc3d1488d36985d9b
BryanBenavides <142626216+BryanBenavides@users.noreply.github.com>
2023-11-17 20:52:37 -0600

    Add files via upload

---
+++ ActaDeNotas/Estudiante.java
@@ -0,0 +1,19 @@
+package ActaDeNotas;
+
+class Estudiante {
+    String carnet;
+    String nombresApellidos;
+    double primerParcial;
+    double segundoParcial;
+    double proyecto;
+    double sistematicos;
+
+    public Estudiante(String carnet, String nombresApellidos, double primerParcial, double segundoParcial, double proyecto, double sistematicos) {
+        this.carnet = carnet;
+        this.nombresApellidos = nombresApellidos;
+        this.primerParcial = primerParcial;
+        this.segundoParcial = segundoParcial;
+        this.proyecto = proyecto;
+        this.sistematicos = sistematicos;
+    }
+}
